f2b6b6f3f9d0932caa3945452fefbedffc2964a2
Matheus adrat <iamadrat@gmail.com>
2022-06-13 21:55:14 -0300

    day2

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,42 +1,58 @@
 import javax.swing.*;
+import java.util.Objects;
 
 public class tictactoe {
+    private static String jogador = "";
+    private static String ultimaJogada;
+    private static String matriz[][] = new String[3][3];
+
+    public static void inicio() {
+        int linha = 0, coluna;
+        jogador = JOptionPane.showInputDialog("Insira o jogador (X || O)");
+        ultimaJogada = jogador;
+
+        linha = Integer.parseInt(JOptionPane.showInputDialog("Informe a linha"));
+        coluna = Integer.parseInt(JOptionPane.showInputDialog("Informe a coluna"));
+        if (!validarJogada(linha, coluna)) {
+            JOptionPane.showMessageDialog(null, "Jogada invalida");
+        }
+    }
+
+    public static boolean validarJogador() {
+        if (Objects.equals(jogador, ultimaJogada)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validarJogada(int linha, int coluna) {
+        try {
+            if (linha < 1 || linha > 3 || coluna < 1 || coluna > 3) {
+                return false;
+
+            }
+            if (matriz[linha][coluna] != null) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+//    public static void pegarLinha(String[] jogador) {
+//        for (int i = 0; i < 9; i++) {
+//            JOptionPane.showInputDialog("vez do jogador: " + jogador[i] + " | Insira a linha");
+//        }
+//    }
+//
+//    public static void pegarColuna(String[] jogador) {
+//        for (int i = 0; i < 9; i++) {
+//            JOptionPane.showInputDialog("vez do jogador: " + jogador[i] + " | Insira a coluna");
+//        }
+//    }
+
     public static void main(String[] args) {
-        pegarJogador(preencheJogador());
+        inicio();
     }
-
-    public static int[][] tabelaJogo() {
-        int matriz[][] = new int[3][3];
-        return matriz;
-    }
-// preencheJogador == Metodo que auto preenche o jogador(X || O)
-    public static String[] preencheJogador() {
-        String jogador[] = new String[9];
-        for (int i = 0; i < 9; i++) {
-            if (i % 2 == 0) {
-                jogador[i] = "X";
-            } else {
-                jogador[i] = "O";
-            }
-        }
-        return jogador;
-    }
-
-    public static void pegarJogador(String[] jogador) {
-        for (int i = 0; i < 9; i++) {
-            JOptionPane.showMessageDialog(null, "vez do jogador: " + jogador[i] + "");
-        }
-    }
-
-    public static void pedirPosicao() {
-        int linha[] = new int[9];
-        int coluna[] = new int[9];
-        for (int l = 0; l < 3; l++) {
-            for (int c = 0; c < 3; c++) {
-                linha[l] = Integer.parseInt(JOptionPane.showInputDialog("Insira a linha desejada"));
-                coluna[c] = Integer.parseInt(JOptionPane.showInputDialog("Insira a coluna desejada"));
-            }
-        }
-    }
-
 }
