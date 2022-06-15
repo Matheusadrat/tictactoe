f830d4652f2099d9340be08b78af9b8c5012c1d5
Matheus adrat <iamadrat@gmail.com>
2022-06-14 22:16:36 -0300

    day3

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,28 +1,40 @@
 import javax.swing.*;
-import java.util.Objects;
 
 public class tictactoe {
     private static String jogador = "";
-    private static String ultimaJogada;
+    private static String ultimoJogador = "";
     private static String matriz[][] = new String[3][3];
+    private static boolean vencedor;
 
     public static void inicio() {
-        int linha = 0, coluna;
-        jogador = JOptionPane.showInputDialog("Insira o jogador (X || O)");
-        ultimaJogada = jogador;
-
-        linha = Integer.parseInt(JOptionPane.showInputDialog("Informe a linha"));
-        coluna = Integer.parseInt(JOptionPane.showInputDialog("Informe a coluna"));
-        if (!validarJogada(linha, coluna)) {
-            JOptionPane.showMessageDialog(null, "Jogada invalida");
-        }
+        int linha = 0, coluna = 0;
+        criarTabela();
+        do {
+            do {
+                jogador = JOptionPane.showInputDialog("Insira o jogador (X || O)");
+            } while (ultimoJogador.equals(jogador));
+            ultimoJogador = jogador;
+            linha = Integer.parseInt(JOptionPane.showInputDialog("Informe a linha"));
+            coluna = Integer.parseInt(JOptionPane.showInputDialog("Informe a coluna"));
+            if (!validarJogada(linha, coluna)) {
+                JOptionPane.showMessageDialog(null, "Jogada invalida");
+            }
+            realizarJogada(linha, coluna);
+            vencedor = validarVencedor();
+        } while (!vencedor);
+        mensagemVencedor();
     }
 
-    public static boolean validarJogador() {
-        if (Objects.equals(jogador, ultimaJogada)) {
-            return false;
+    public static void realizarJogada(int linha, int coluna) {
+        matriz[linha - 1][coluna - 1] = jogador;
+    }
+
+    public static void criarTabela() {
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+                matriz[l][c] = "";
+            }
         }
-        return true;
     }
 
     public static boolean validarJogada(int linha, int coluna) {
@@ -31,7 +43,7 @@ public class tictactoe {
                 return false;
 
             }
-            if (matriz[linha][coluna] != null) {
+            if (!matriz[linha - 1][coluna - 1].equals("")) {
                 return false;
             }
         } catch (Exception e) {
@@ -40,17 +52,29 @@ public class tictactoe {
         return true;
     }
 
-//    public static void pegarLinha(String[] jogador) {
-//        for (int i = 0; i < 9; i++) {
-//            JOptionPane.showInputDialog("vez do jogador: " + jogador[i] + " | Insira a linha");
-//        }
-//    }
-//
-//    public static void pegarColuna(String[] jogador) {
-//        for (int i = 0; i < 9; i++) {
-//            JOptionPane.showInputDialog("vez do jogador: " + jogador[i] + " | Insira a coluna");
-//        }
-//    }
+    public static boolean validarVencedor() {
+        for (int l = 0; l < 3; l++) {
+            int contadora = 0;
+            for (int c = 0; c < 3; c++) {
+                if (matriz[l][c].equals("X")) {
+                    contadora += 1;
+                    continue;
+                }
+                if (matriz[l][c].equals("O")) {
+                    contadora += 1;
+                }
+
+            }
+            if (contadora == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void mensagemVencedor() {
+        JOptionPane.showMessageDialog(null, "O jogador: " + ultimoJogador + " Venceu!");
+    }
 
     public static void main(String[] args) {
         inicio();
