5875640697ee2390c2e45544b2559e5c8f95cddc
Matheus adrat <iamadrat@gmail.com>
2022-06-15 22:06:53 -0300

    finalized

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,28 +1,44 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 
 public class tictactoe {
     private static String jogador = "";
     private static String ultimoJogador = "";
     private static String matriz[][] = new String[3][3];
-    private static boolean vencedor;
+    private static int resultado;
 
     public static void inicio() {
         int linha = 0, coluna = 0;
         criarTabela();
         do {
-            do {
-                jogador = JOptionPane.showInputDialog("Insira o jogador (X || O)");
-            } while (ultimoJogador.equals(jogador));
+             do {
+            jogador = JOptionPane.showInputDialog("Insira o jogador (X || O)").toUpperCase();
+            } while (ultimoJogador.equals(jogador) || !jogador.equals("X") && !jogador.equals("O"));
             ultimoJogador = jogador;
             linha = Integer.parseInt(JOptionPane.showInputDialog("Informe a linha"));
             coluna = Integer.parseInt(JOptionPane.showInputDialog("Informe a coluna"));
             if (!validarJogada(linha, coluna)) {
                 JOptionPane.showMessageDialog(null, "Jogada invalida");
+                continue;
             }
             realizarJogada(linha, coluna);
-            vencedor = validarVencedor();
-        } while (!vencedor);
-        mensagemVencedor();
+            mostrarTabela();
+            resultado = validarVencedor();
+        } while (resultado == 2);
+        mensagemResultado(resultado);
+    }
+
+    public static void mostrarTabela() {
+        String[] colunas = {"", "", ""};
+        JTable table = new JTable(matriz, colunas);
+        table.setTableHeader(null);
+        DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+        dtcr.setHorizontalAlignment(JLabel.CENTER);
+
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            table.setDefaultRenderer(table.getColumnClass(i), dtcr);
+        }
+        JOptionPane.showMessageDialog(null, new JScrollPane(table));
     }
 
     public static void realizarJogada(int linha, int coluna) {
@@ -52,28 +68,49 @@ public class tictactoe {
         return true;
     }
 
-    public static boolean validarVencedor() {
+    public static int validarVencedor() {
+        int contadoraEmpate = 0;
         for (int l = 0; l < 3; l++) {
-            int contadora = 0;
+            int contadoraLinha = 0, contadoraColuna = 0;
             for (int c = 0; c < 3; c++) {
-                if (matriz[l][c].equals("X")) {
-                    contadora += 1;
-                    continue;
+                if (matriz[l][c].equals(ultimoJogador)) {
+                    contadoraLinha += 1;
                 }
-                if (matriz[l][c].equals("O")) {
-                    contadora += 1;
+                if (contadoraLinha == 3) {
+                    return 1;
+                }
+                if (matriz[c][l].equals(ultimoJogador)) {
+
+                    contadoraColuna += 1;
+                }
+                if (contadoraColuna == 3) {
+                    return 1;
+                }
+                if (matriz[0][0].equals(ultimoJogador) && matriz[1][1].equals(ultimoJogador) && matriz[2][2].equals(ultimoJogador)) {
+                    return 1;
                 }
 
-            }
-            if (contadora == 3) {
-                return true;
+                if (matriz[0][2].equals(ultimoJogador) && matriz[1][1].equals(ultimoJogador) && matriz[2][0].equals(ultimoJogador)) {
+                    return 1;
+                }
+                if (!matriz[l][c].equals("")) {
+                    contadoraEmpate += 1;
+                    if (contadoraEmpate == 9) {
+                        return 0;
+                    }
+                }
             }
         }
-        return false;
+        return 2;
     }
 
-    public static void mensagemVencedor() {
-        JOptionPane.showMessageDialog(null, "O jogador: " + ultimoJogador + " Venceu!");
+
+    public static void mensagemResultado(int resultado) {
+        if (resultado == 0) {
+            JOptionPane.showMessageDialog(null, "VELHA");
+        } else {
+            JOptionPane.showMessageDialog(null, "O jogador: " + ultimoJogador + " Venceu!");
+        }
     }
 
     public static void main(String[] args) {
